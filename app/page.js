6d662dfbff995ffa68888190278b5971296fa895
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-07-26 16:21:33 +0530

    update

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   
 
   return (
-    <div className="bg-white text-black">
+    <div className="bg-white c text-black">
        {/* <NavBar/> */}
        <HeroPage/>
     </div>
